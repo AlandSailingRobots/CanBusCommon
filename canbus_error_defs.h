0af18a69922dfd0e99fae1fa789f75e3aacb3715
Dag Karlsson <dag.karlsson@ha.ax>
2018-04-24 13:33:04 +0300

    Added extra error messages

--- canbus_error_defs.h
+++ canbus_error_defs.h
@@ -1,15 +1,16 @@
 /****************************************************************************************
-*
-* File:
-* 		canbus_error_defs.h
-*
-* Purpose:
-*		 The purpose of this definitions is a unified use of error codes
+ *
+ * File:
+ * 		canbus_error_defs.h
+ *
+ * Purpose:
+ *		 The purpose of this definitions is a unified use of error codes
  *		 between Arduino and RPI.
-*
-* Developer Notes:
-*
-***************************************************************************************/
+ *
+ * Developer Notes:
+ *
+ ***************************************************************************************/
+// clang-format off
 
 #ifndef CANBUS_ERROR_DEFS_H
 #define CANBUS_ERROR_DEFS_H
@@ -17,16 +18,20 @@
 
 #define NO_ERRORS 0
 
-#define ERROR_SENSOR_PH_SYNTAX 1                    // Syntax error. Happens if we send wrong command to sensor
-#define ERROR_SENSOR_PH_NOT_READY 2                 // Not ready. Means we did not wait long enough for sensor to actually get a reading
-#define ERROR_SENSOR_PH_NO_DATA 3                   // No data from sensor. Probably sensor failure? Check specifications from sensor documentation
+#define ERROR_SENSOR_PH_NO_CONNECTION 1             // No connection to sensor unit
+#define ERROR_SENSOR_PH_SYNTAX 2                    // Syntax error. Happens if we send wrong command to sensor
+#define ERROR_SENSOR_PH_NOT_READY 3                 // Not ready. Means we did not wait long enough for sensor to actually get a reading
+#define ERROR_SENSOR_PH_NO_DATA 4                   // No data from sensor. Probably sensor failure? Check specifications from sensor documentation
 
-#define ERROR_SENSOR_CONDUCTIVETY_SYNTAX 4          // Syntax error. Happens if we send wrong command to sensor
-#define ERROR_SENSOR_CONDUCTIVETY_NOT_READY 5       // Not ready. Means we did not wait long enough for sensor to actually get a reading
-#define ERROR_SENSOR_CONDUCTIVETY_NO_DATA 6         // No data from sensor. Probably sensor failure? Check specifications from sensor documentation
+#define ERROR_SENSOR_CONDUCTIVETY_NO_CONNECTION 5   // No connection to sensor unit
+#define ERROR_SENSOR_CONDUCTIVETY_SYNTAX 6          // Syntax error. Happens if we send wrong command to sensor
+#define ERROR_SENSOR_CONDUCTIVETY_NOT_READY 7       // Not ready. Means we did not wait long enough for sensor to actually get a reading
+#define ERROR_SENSOR_CONDUCTIVETY_NO_DATA 8         // No data from sensor. Probably sensor failure? Check specifications from sensor documentation
 
-#define ERROR_SENSOR_TEMPERATURE_SYNTAX 7           // Syntax error. Happens if we send wrong command to sensor
-#define ERROR_SENSOR_TEMPERATURE_NOT_READY 8        // Not ready. Means we did not wait long enough for sensor to actually get a reading
-#define ERROR_SENSOR_TEMPERATURE_NO_DATA 9          // No data from sensor. Probably sensor failure? Check specifications from sensor documentation
+#define ERROR_SENSOR_TEMPERATURE_NO_CONNECTION 9     // No connection to sensor unit
+#define ERROR_SENSOR_TEMPERATURE_SYNTAX 10           // Syntax error. Happens if we send wrong command to sensor
+#define ERROR_SENSOR_TEMPERATURE_NOT_READY 11        // Not ready. Means we did not wait long enough for sensor to actually get a reading
+#define ERROR_SENSOR_TEMPERATURE_NO_DATA 12          // No data from sensor. Probably sensor failure? Check specifications from sensor documentation
 
 #endif
+// clang-format on
